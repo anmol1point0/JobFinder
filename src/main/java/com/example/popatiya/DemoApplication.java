35c1bd2021a3d1f5c98fbefc1f512cabbfc69679
Anmol Mittal <anmol15554@gmail.com>
2020-12-27 19:38:36 +0530

    everything is done with database embedded

--- src/main/java/com/example/popatiya/DemoApplication.java
+++ src/main/java/com/example/popatiya/DemoApplication.java
@@ -2,32 +2,13 @@ package com.example.popatiya;
 
 import java.io.IOException;
 
-import javax.annotation.PostConstruct;
-
-import com.example.popatiya.datafetch.dataget;
-import com.example.popatiya.repository.myrepo;
-
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
 
-    // @Autowired
-    // private static dataget Data;
-    // @Autowired
-    // private static myrepo myRepo;
-
-    public static void main(String[] arg) throws IOException {
-        // myrepo myRepo = new myrepo();
-        dataget Data = new dataget();
-        Data.runMethod();
-        // SpringApplication.run(DemoApplication.class, arg);
-
+    public static void main(String args[]) {
+        SpringApplication.run(DemoApplication.class, args);
     }
-
-    // @PostConstruct
-    // private void init() {
-    // Data.runMethod();
-    // // ...
-    // }
 }
